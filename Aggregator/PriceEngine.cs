053023eae954f8e8747a717f7028409da6039d23
Adnan Hussain <axhussain@gmail.com>
2023-04-16 11:18:49 +0100

    Use Pattern Matching to choose which system to call

--- Aggregator/PriceEngine.cs
+++ Aggregator/PriceEngine.cs
@@ -6,79 +6,99 @@ namespace Aggregator;
 public class PriceEngine
 {
     private readonly PriceRequest _request;
+    private readonly List<object> _quotationSystems = new();
 
     //Pass request with risk data with details of a gadget, return the best price retrieved from 3 external quotation engines
     public PriceEngine(PriceRequest request)
     {
         EnsureArg.IsNotNull(request, nameof(request));
-
         _request = request;
+
+        RegisterQuotationSystems();
+    }
+
+    private void RegisterQuotationSystems()
+    {
+        _quotationSystems.Add(new QuotationSystem1("http://quote-system-1.com", "1234"));
+        dynamic systemRequest2 = BuildSystem2Request();
+        _quotationSystems.Add(new QuotationSystem2("http://quote-system-2.com", "1235", systemRequest2));
+        _quotationSystems.Add(new QuotationSystem3("http://quote-system-3.com", "100"));
     }
 
     public PriceResponse GetPrice()
     {
-        List<PriceResponse> responses = new();
+        var responses = _quotationSystems.Select(x => GetQuotation(x)).ToList();
 
-        //System 1 requires DOB to be specified
-        if (RequestHasDoB())
-        {
-            QuotationSystem1 system1 = new QuotationSystem1("http://quote-system-1.com", "1234");
-            dynamic systemRequest1 = BuildSystem1Request();
+        var response = responses?.Where(x => x != null).OrderBy(x => x!.Price).FirstOrDefault();
 
-            dynamic system1Response = system1.GetPrice(systemRequest1);
-
-            if (system1Response.IsSuccess)
-            {
-                responses.Add(new PriceResponse
-                {
-                    Price = system1Response.Price,
-                    Insurer = system1Response.Name,
-                    Tax = system1Response.Tax
-                });
-            }
-        }
-
-        //System 2 will only provide quotes for some makes
-        if (RequestHasSystem2Makes())
-        {
-            dynamic systemRequest2 = BuildSystem2Request();
-            QuotationSystem2 system2 = new QuotationSystem2("http://quote-system-2.com", "1235", systemRequest2);
-
-            dynamic system2Response = system2.GetPrice();
-
-            if (system2Response.HasPrice)
-            {
-                responses.Add(new PriceResponse
-                {
-                    Price = system2Response.Price,
-                    Insurer = system2Response.Name,
-                    Tax = system2Response.Tax
-                });
-            }
-        }
-
-        //System 3 has not limitations on what it can quote for
-        QuotationSystem3 system3 = new QuotationSystem3("http://quote-system-3.com", "100");
-        dynamic systemRequest3 = BuildSystem3Request();
-
-        var system3Response = system3.GetPrice(systemRequest3);
-
-        if (system3Response.IsSuccess)
-        {
-            responses.Add(new PriceResponse
-            {
-                Price = system3Response.Price,
-                Insurer = system3Response.Name,
-                Tax = system3Response.Tax
-            });
-        }
-
-        var response = responses.OrderBy(x => x.Price).FirstOrDefault();
         return response ?? new PriceResponse()
         {
             Error = "No quotes found.",
             Price = -1
         };
+    }
+
+    private PriceResponse? GetQuotation(object quotationSystem)
+    {
+        var response = quotationSystem switch
+        {
+            QuotationSystem1 system1 when RequestHasDoB() => CallSystem1(system1),
+            QuotationSystem2 system2 when RequestHasSystem2Makes() => CallSystem2(system2),
+            QuotationSystem3 system3 => CallSystem3(system3),
+            _ => null
+        };
+
+        return response;
+    }
+
+    private PriceResponse? CallSystem1(QuotationSystem1 system1)
+    {
+        dynamic systemRequest1 = BuildSystem1Request();
+        dynamic system1Response = system1.GetPrice(systemRequest1);
+
+        if (system1Response.IsSuccess)
+        {
+            return new PriceResponse
+            {
+                Price = system1Response.Price,
+                Insurer = system1Response.Name,
+                Tax = system1Response.Tax
+            };
+        }
+        return null;
+    }
+
+    private PriceResponse? CallSystem2(QuotationSystem2 system2)
+    {
+        dynamic system2Response = system2.GetPrice();
+
+        if (system2Response.HasPrice)
+        {
+            return new PriceResponse
+            {
+                Price = system2Response.Price,
+                Insurer = system2Response.Name,
+                Tax = system2Response.Tax
+            };
+        }
+        return null;
+    }
+
+    private PriceResponse? CallSystem3(QuotationSystem3 system3)
+    {
+        dynamic systemRequest3 = BuildSystem3Request();
+        var system3Response = system3.GetPrice(systemRequest3);
+
+        if (system3Response.IsSuccess)
+        {
+            return new PriceResponse
+            {
+                Price = system3Response.Price,
+                Insurer = system3Response.Name,
+                Tax = system3Response.Tax
+            };
+        }
+        return null;
     }
 
     private bool RequestHasDoB()
@@ -88,9 +108,14 @@ public class PriceEngine
 
     private bool RequestHasSystem2Makes()
     {
-        return _request.RiskData.Make == "examplemake1"
-            || _request.RiskData.Make == "examplemake2"
-            || _request.RiskData.Make == "examplemake3";
+        var validMakes = new List<string>
+        {
+            "examplemake1",
+            "examplemake2",
+            "examplemake3"
+        };
+
+        return validMakes.Contains(_request.RiskData.Make ?? "");
     }
 
     private dynamic BuildSystem1Request()
